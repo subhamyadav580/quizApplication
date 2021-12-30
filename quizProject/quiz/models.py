f7dd84022bfd9feb3ed36a980bcb0df7e4d23dc2
subhamyadav580 <subhamyadav580@gmail.com>
2021-12-30 19:56:14 +0530

    adding project directory

---
+++ quizProject/quiz/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    is_teacher = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
+
